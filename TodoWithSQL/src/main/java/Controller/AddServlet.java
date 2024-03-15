7d24ebf802a86dc9cb1fb2ceb4a47aa5b9d602a9
Shunsuke Kuzawa <98546409+skuzawa@users.noreply.github.com>
2024-03-15 13:28:11 +0900

    Add files via upload

--- TodoWithSQL/src/main/java/Controller/AddServlet.java
+++ TodoWithSQL/src/main/java/Controller/AddServlet.java
@@ -1,20 +1,17 @@
 package Controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.sql.SQLException;
+import java.util.Objects;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Model.ListDAO;
 
 /**
  * Servlet implementation class TodoController
@@ -24,144 +21,73 @@ public class AddServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		String url = "jdbc:mysql://localhost/todo";
-		String user = "root";
-		String password = "password";
+		request.setCharacterEncoding("UTF-8");
+		//時系列順に並べ替え（default:null）
+		String dateSort = request.getParameter("dateSort");
+		//優先度順に並べ替え（default:null）
+		String priorSort = request.getParameter("priorSort");
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (Exception e) {
+			ListDAO dao = new ListDAO();
+			//データの一覧表示
+			request.setAttribute("rows", dao.select(dateSort, priorSort));
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			// リクエストスコープへのデータ格納（リストデータの格納）
+			request.setAttribute("message", "例外が発生しました");
+			String view = "WEB-INF/view/error.jsp";
+			// 転送オブジェクトを取得
+			RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+			// 転送
+			dispatcher.forward(request, response);
 		}
-		String sql = "SELECT * FROM posts where del_flag = 0";
-		String sortType = request.getParameter("sortType");
-		String priorType = request.getParameter("priorType");
-		
-		if(sortType == null) {
-			;
-		}else if(sortType.equals("upSort")){
-			sql = "SELECT * FROM posts where del_flag = 0 ORDER BY create_time DESC";
-		}else if(sortType.equals("downSort")) {
-			sql = "SELECT * FROM posts where del_flag = 0 ORDER BY create_time ASC";
-		}else{
-			;
-		}
-		
-		if(priorType == null) {
-			;
-		}else if(priorType.equals("upSort")){
-			sql = "SELECT * FROM posts where del_flag = 0 ORDER BY prior ASC";
-		}else if(priorType.equals("downSort")) {
-			sql = "SELECT * FROM posts where del_flag = 0 ORDER BY prior DESC";
-		}else{
-			;
-		}
-		
-        try (Connection connection = DriverManager.getConnection
-        (url,user,password);
-        PreparedStatement statement = connection.prepareStatement(sql);
-        ResultSet results = statement.executeQuery()){
-            ArrayList<HashMap<String,String>> rows = new ArrayList<HashMap<String,String>>();
-
-            while(results.next()){
-                HashMap<String,String>columns = new HashMap<String,String>();
-
-                String id = results.getString("id");
-                columns.put("id",id);
-                
-                String task_name = results.getString("title");
-                columns.put("task_name",task_name);
-
-                String content = results.getString("content");
-                columns.put("content",content);
-                
-                String prior = results.getString("prior");
-                columns.put("prior",prior);
-
-                rows.add(columns);
-            }
-            request.setAttribute("rows",rows);
-        }catch(Exception e){
-            request.setAttribute("message","Exception:"+ e.getMessage());
-		}
-
 		String view = "/WEB-INF/views/top.jsp";
 		request.getRequestDispatcher(view).forward(request, response);
 	}
-	
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		request.setCharacterEncoding("UTF-8");
-		//todo.jsp→入力値
+	
 		String task_name = request.getParameter("task_name");
 		String content = request.getParameter("content");
-		String prior =  request.getParameter("prior");
+		String prior = request.getParameter("prior");
 		String action = request.getParameter("action");
 		String task_id = request.getParameter("task_id");
 		
-		Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
-		String currentTimestampToString = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(currentTimestamp);
-		
-		String url = "jdbc:mysql://localhost/todo";
-		String user = "root";
-		String password = "password";
-
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		if(action.equals("delete")){
-			String sql = "UPDATE posts SET del_flag=1 WHERE id=?";
-			Connection con = null;
-			PreparedStatement ps = null;
+		//削除ボタンか保存ボタンのリクエストか条件分岐
+		if (Objects.equals(action,"delete")) {
 			try {
-				con = DriverManager.getConnection(url, user, password);
-				ps = con.prepareStatement(sql);
-				ps.setString(1, task_id);
-				ps.executeUpdate();
-			} catch (Exception e) {
+				ListDAO dao = new ListDAO();
+				dao.delete(task_id);
+			} catch (SQLException e) {
 				e.printStackTrace();
-			} finally {
-				try {
-					//⑤リソースを解放
-					if (ps != null)
-						ps.close();
-					if (con != null)
-						con.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			} catch (Exception e) {
+				// リクエストスコープへのデータ格納（リストデータの格納）
+				request.setAttribute("message", "例外が発生しました");
+				String view = "WEB-INF/view/error.jsp";
+				// 転送オブジェクトを取得
+				RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+				// 転送
+				dispatcher.forward(request, response);
 			}
-		}else {
-			String sql = "INSERT INTO posts (title,content,create_time,delete_time,prior) VALUES (?,?,?,?,?)";
-			Connection con = null;
-			PreparedStatement ps = null;
+		} else {
+			//保存ボタンからリクエスト
 			try {
-				con = DriverManager.getConnection(url, user, password);
-				ps = con.prepareStatement(sql);
-				ps.setString(1, task_name);
-				ps.setString(2, content);
-				ps.setString(3, currentTimestampToString);
-				ps.setString(4, currentTimestampToString);
-				ps.setString(5, prior);
-				ps.executeUpdate();
-			} catch (Exception e) {
+				ListDAO dao = new ListDAO();
+				dao.insert(task_name, content, prior,action, task_id);
+			} catch (SQLException e) {
 				e.printStackTrace();
-			} finally {
-				try {
-					//⑤リソースを解放
-					if (ps != null)
-						ps.close();
-					if (con != null)
-						con.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			} catch (Exception e) {
+				// リクエストスコープへのデータ格納（リストデータの格納）
+				request.setAttribute("message", "例外が発生しました");
+				String view = "WEB-INF/view/error.jsp";
+				// 転送オブジェクトを取得
+				RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+				// 転送
+				dispatcher.forward(request, response);
 			}
 		}
 		doGet(request, response);
--- TodoWithSQL/src/main/webapp/WEB-INF/views/top.jsp
+++ TodoWithSQL/src/main/webapp/WEB-INF/views/top.jsp
@@ -6,6 +6,7 @@
 <head>
 <meta charset="UTF-8">
 <title>タスク管理へ、ようこそ</title>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
 </head>
 <body>
 	<form action="todo" method="POST">
@@ -22,13 +23,13 @@
 	</form>
 	<label>時間順</label>
 	<form action="todo" method="GET">
-		<button type="submit" value="upSort" class="btn" name="sortType">昇順</button>
-		<button type="submit" value="downSort" class="btn" name="sortType">降順</button>
+		<button type="submit" value="upSort" class="btn" name="dateSort">昇順</button>
+		<button type="submit" value="downSort" class="btn" name="dateSort">降順</button>
 	</form>
 	<label>優先度順</label>
 	<form action="todo" method="GET">
-		<button type="submit" value="upSort" class="btn" name="priorType">昇順</button>
-		<button type="submit" value="downSort" class="btn" name="priorType">降順</button>
+		<button type="submit" value="upSort" class="btn" name="priorSort">昇順</button>
+		<button type="submit" value="downSort" class="btn" name="priorSort">降順</button>
 	</form>
 	<%
 	ArrayList<HashMap<String, String>> rows = (ArrayList<HashMap<String, String>>) request.getAttribute("rows");
@@ -44,16 +45,45 @@
 		%>
 		<tr>
 		<form action="todo" method="POST">
-			<td><%=columns.get("task_name")%></td>
+			<td><dd id=<%=columns.get("id")%>><%=columns.get("task_name")%></dd></td>
 			<td><%=columns.get("content")%></td>
 			<td><%=columns.get("prior")%></td>
-			
-				<td><button type="submit" value="delete" class="btn"
-						name="action">削除</button></td> <input type="hidden" name="task_id"
-					value=<%=columns.get("id")%>>
+			<td><button type="submit" value="delete" class="btn"　name="action">削除</button></td> 
+			<input type="hidden" name="task_id"　value=<%=columns.get("id")%>>
+			<td><button type="submit" value="update" class="btn"　name="action">更新</button></td> 
+			<input type="hidden" name="task_id"　value=<%=columns.get("id")%>>
 			</form>
 		</tr>
 		<%}%>
 	</table>
 </body>
+<script>
+jQuery(function($){
+    $('dd').click(function(){
+        //classでonを持っているかチェック
+        if(!$(this).hasClass('on')){
+            //編集可能時はclassでonをつける
+            $(this).addClass('on');
+            var txt = $(this).text();
+            //テキストをinputのvalueに入れてで置き換え
+            $(this).html('<input type="text" value="'+txt+'" />');
+            //同時にinputにフォーカスをする
+            $('dd > input').focus().blur(function(){
+                var inputVal = $(this).val();
+                //もし空欄だったら空欄にする前の内容に戻す
+                if(inputVal===''){
+                    inputVal = this.defaultValue;
+                };
+                var id = $(this).prop("id");
+                //編集が終わったらtextで置き換える
+                $(this).parent().removeClass('on').text(inputVal);
+                
+                //$(this).parent().removeClass('on').getElementById("").setAttribute("value",inputVal); 
+            });
+        };
+    });
+});
+</script>
+
+
 </html>
